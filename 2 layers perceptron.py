05aea983fc4224aa9461c64d98655c4f18e43b43
Suchana Chakrabarti <48732052+Suchana34@users.noreply.github.com>
2020-01-24 21:07:22 +0530

    Create 2 layers perceptron.py

---
+++ 2 layers perceptron.py
@@ -0,0 +1,24 @@
+def predict_with_network(input_data):
+
+    node_0_0_input = (input_data* weights['node_0_0']).sum()
+    node_0_0_output = relu(node_0_0_input)
+
+    node_0_1_input = (input_data*weights['node_0_1']).sum()
+    node_0_1_output = relu(node_0_1_input)
+
+    hidden_0_outputs = np.array([node_0_0_output, node_0_1_output])
+    
+    node_1_0_input = (hidden_0_outputs*weights['node_1_0']).sum()
+    node_1_0_output = relu(node_1_0_input)
+
+    node_1_1_input = (hidden_0_outputs*weights['node_1_1']).sum()
+    node_1_1_output = relu(node_1_1_input)
+
+    hidden_1_outputs = np.array([node_1_0_output, node_1_1_output])
+
+    model_output = (hidden_1_outputs*weights['output']).sum()
+    
+    return(model_output)
+
+output = predict_with_network(input_data)
+print(output)
